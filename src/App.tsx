2dec4ada724a160ee5ba7ed79e1c152055998790
serhatprogramming <serhatprogramming@gmail.com>
2023-05-15 16:42:40 -0400

    json server added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 interface Note {
   id: number;
@@ -7,9 +8,40 @@ interface Note {
 
 const App = () => {
   const [newNote, setNewNote] = useState("");
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<Note[]>([{ id: 1, content: "testing" }]);
 
-  return null;
+  useEffect(() => {
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
+  const noteCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const noteToAdd = {
+      content: newNote,
+      id: notes.length + 1,
+    };
+    setNotes(notes.concat(noteToAdd));
+    setNewNote("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={noteCreation}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
